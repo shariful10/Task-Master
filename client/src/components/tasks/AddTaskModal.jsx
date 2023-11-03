7c53b3722bdec77edb1aa3b1a39cc0e6362a4488
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-11-03 18:50:13 +0600

    Cancel Functionality Implemented On Modal

--- client/src/components/tasks/AddTaskModal.jsx
+++ client/src/components/tasks/AddTaskModal.jsx
@@ -2,16 +2,22 @@ import { Modal } from "../ui/Modal";
 import { useForm } from "react-hook-form";
 
 const AddTaskModal = ({ isOpen, setIsOpen }) => {
-	const { register, handleSubmit } = useForm();
+	const { register, handleSubmit, reset } = useForm();
 
 	const onSubmit = (data) => {
 		console.log(data);
+		onCancel();
+	};
+
+	const onCancel = () => {
+		reset();
+		setIsOpen(false);
 	};
 
 	return (
 		<Modal isOpen={isOpen} setIsOpen={setIsOpen} title="Programming Hero">
 			<form onSubmit={handleSubmit(onSubmit)}>
-				<div className="flex flex-col gap-3">
+				<div className="flex flex-col gap-3 mb-5">
 					<label htmlFor="title">Title</label>
 					<input
 						type="text"
@@ -20,7 +26,77 @@ const AddTaskModal = ({ isOpen, setIsOpen }) => {
 						{...register("name")}
 					/>
 				</div>
-				<button type="submit">Submit</button>
+				<div className="flex flex-col mb-5">
+					<label htmlFor="description" className="mb-2">
+						Description
+					</label>
+					<textarea
+						type="text"
+						id="description"
+						className="w-full rounded-md"
+						{...register("description")}
+					/>
+				</div>
+				<div className="flex flex-col mb-5">
+					<label htmlFor="description" className="mb-2">
+						Deadline
+					</label>
+					<input
+						type="date"
+						id="date"
+						className="w-full rounded-md"
+						{...register("date")}
+					/>
+				</div>
+				<div className="flex flex-col mb-5">
+					<label htmlFor="title" className="mb-2">
+						Assign to
+					</label>
+					<select
+						id="assignTo"
+						className="w-full rounded-md"
+						{...register("assignTo")}
+					>
+						<option value="Shariful Islam">Shariful Islam</option>
+						<option value="Mir Hossain">Mir Hossain</option>
+						<option value="Nahid Hossain">Nahid Hossain</option>
+						<option value="Mizanur Rahman">Mizanur Rahman</option>
+						<option value="Tanmoy Parvez">Tanmoy Parvez</option>
+						<option value="Fahim Ahmed">Fahim Ahmed</option>
+						<option value="Rahatul Islam">Rahatul Islam</option>
+						<option value="Samim Israr Ravi">Samim Israr Ravi</option>
+						<option value="Mehedi Anik">Mehedi Anik</option>
+						<option value="Eshtiaque Ahmed">Eshtiaque Ahmed</option>
+						<option value="Tarique Ahmed">Tarique Ahmed</option>
+						<option value="Masud Alam">Masud Alam</option>
+					</select>
+				</div>
+				<div className="flex flex-col mb-5">
+					<label htmlFor="title" className="mb-2">
+						Priority
+					</label>
+					<select
+						id="priority"
+						className="w-full rounded-md"
+						{...register("priority")}
+					>
+						<option value="high">High</option>
+						<option value="medium">Medium</option>
+						<option value="low">Low</option>
+					</select>
+				</div>
+				<div className="flex gap-3 justify-end">
+					<button
+						onClick={() => onCancel()}
+						type="button"
+						className="btn btn-danger"
+					>
+						Cancel
+					</button>
+					<button type="submit" className="btn btn-primary">
+						Submit
+					</button>
+				</div>
 			</form>
 		</Modal>
 	);
