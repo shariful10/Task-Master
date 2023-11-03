f39a5bf0c36a5737c131c9845a24c560de59f0b4
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-11-03 15:47:54 +0600

    Headless UI Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import MainLayout from './components/layouts/MainLayout';
+import MainLayout from "./components/layouts/MainLayout";
 
 function App() {
-  return (
-    <>
-      <MainLayout />
-    </>
-  );
+	return (
+		<>
+			<MainLayout />
+		</>
+	);
 }
 
 export default App;
--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,9 +1,9 @@
 const Chat = () => {
-  return (
-    <div>
-      <h1>Chat</h1>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Chat</h1>
+		</div>
+	);
 };
 
 export default Chat;
