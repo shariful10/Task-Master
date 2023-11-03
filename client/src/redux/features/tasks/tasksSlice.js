b974376261ce367354796fd9b3cd7635a0d7b324
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-11-03 19:48:51 +0600

    Status Implemented On addTask Function

--- client/src/redux/features/tasks/tasksSlice.js
+++ client/src/redux/features/tasks/tasksSlice.js
@@ -10,10 +10,14 @@ const taskSlice = createSlice({
 	reducers: {
 		addTask: (state, { payload }) => {
 			if (state.tasks.length === 0) {
-				state.tasks.push({ id: 1, ...payload });
+				state.tasks.push({ id: 1, status: "painding", ...payload });
 			} else {
 				const lastElement = state.tasks.at(-1);
-				state.tasks.push({ id: lastElement.id + 1, ...payload });
+				state.tasks.push({
+					id: lastElement.id + 1,
+					status: "painding",
+					...payload,
+				});
 			}
 		},
 	},
