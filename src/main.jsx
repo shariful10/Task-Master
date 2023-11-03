3bdb43ce85026e7d86f40c2e2670f48dd3c79267
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-11-03 15:11:41 +0600

    Redux Store Connected

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { RouterProvider } from 'react-router-dom';
-import routes from './routes/routes.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import store from "./redux/store";
+import { Provider } from "react-redux";
+import routes from "./routes/routes.jsx";
+import { RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={routes} />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<Provider store={store}>
+			<RouterProvider router={routes} />
+		</Provider>
+	</React.StrictMode>
 );
