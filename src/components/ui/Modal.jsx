502592c89d111949113b715cf6dbb99dd3ae1e5d
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-11-03 17:19:30 +0600

    Title Implemented On Modal

--- src/components/ui/Modal.jsx
+++ src/components/ui/Modal.jsx
@@ -1,7 +1,7 @@
 import { Dialog, Transition } from "@headlessui/react";
 import { Fragment } from "react";
 
-export const Modal = ({ isOpen, setIsOpen }) => {
+export const Modal = ({ isOpen, setIsOpen, title, children }) => {
 	function closeModal() {
 		setIsOpen(false);
 	}
@@ -38,25 +38,9 @@ export const Modal = ({ isOpen, setIsOpen }) => {
 										as="h3"
 										className="text-lg font-medium leading-6 text-gray-900"
 									>
-										Payment successful
+										{title}
 									</Dialog.Title>
-									<div className="mt-2">
-										<p className="text-sm text-gray-500">
-											Your payment has been successfully submitted.
-											We've sent you an email with all of the details
-											of your order.
-										</p>
-									</div>
-
-									<div className="mt-4">
-										<button
-											type="button"
-											className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-											onClick={closeModal}
-										>
-											Got it, thanks!
-										</button>
-									</div>
+									{children}
 								</Dialog.Panel>
 							</Transition.Child>
 						</div>
--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -22,12 +22,14 @@ const Tasks = () => {
 							<BellIcon className="h-6 w-6" />
 						</button>
 						<button
-							onClick={() => setIsOpen(!isOpen)}
+							onClick={() => setIsOpen(true)}
 							className="btn btn-primary"
 						>
 							Add Task
 						</button>
-						<Modal isOpen={isOpen} setIsOpen={setIsOpen} />
+						<Modal isOpen={isOpen} setIsOpen={setIsOpen} title="Programming Hero">
+                     gyuqguyguq3yr4
+                  </Modal>
 						<div className="h-10 w-10 rounded-xl overflow-hidden">
 							<img
 								src="https://images.unsplash.com/photo-1528892952291-009c663ce843?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=644&q=80"
