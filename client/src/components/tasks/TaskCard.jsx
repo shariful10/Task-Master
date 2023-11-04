a4d7a88aaa29a7567175a53c0089699982222c6a
Sk Shariful Islam <98546214+shariful10@users.noreply.github.com>
2023-11-05 02:21:53 +0600

    Update Status Functionality Implemented On Update Button

--- client/src/components/tasks/TaskCard.jsx
+++ client/src/components/tasks/TaskCard.jsx
@@ -1,19 +1,21 @@
 import { ArrowRightIcon, TrashIcon } from "@heroicons/react/24/outline";
+import { useDispatch } from "react-redux";
+import { updateStatus } from "../../redux/features/tasks/tasksSlice";
 
-const TaskCard = ({task}) => {
-
+const TaskCard = ({ task }) => {
+	const dispatch = useDispatch();
 	return (
 		<div className="bg-secondary/10 rounded-md p-5">
 			<h1 className="text-lg font-semibold mb-3">{task?.title}</h1>
 			<p className="mb-3">{task?.description}</p>
 			<p className="text-sm">Assigned to - {task?.assignedTo}</p>
 			<div className="flex justify-between mt-3">
-				<p>{task?.date}</p>
+				<p className="text-blue-800">{task?.date}</p>
 				<div className="flex gap-3">
 					<button title="Delete">
 						<TrashIcon className="h-5 w-5 text-red-500" />
 					</button>
-					<button title="Update Status">
+					<button onClick={() => dispatch(updateStatus(task.id))} title="Update Status">
 						<ArrowRightIcon className="h-5 w-5 text-primary" />
 					</button>
 				</div>
